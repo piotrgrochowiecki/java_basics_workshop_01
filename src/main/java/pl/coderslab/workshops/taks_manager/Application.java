7d4140a183c8dfde06910658137ea57da279fe07
piotrgrochowiecki <piotrgrochowiecki@gmail.com>
2021-12-17 15:26:34 +0100

    Method taskReader added. Name of class changed to Methods.

--- src/main/java/pl/coderslab/workshops/taks_manager/Application.java
+++ src/main/java/pl/coderslab/workshops/taks_manager/Application.java
@@ -1,9 +1,20 @@
 package pl.coderslab.workshops.taks_manager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
-        Options.optionsDisplay();
+        Methods.optionsDisplay();
+        String[][] tasks = Methods.tasksReader("tasks.csv");
     }
 
 }
---
+++ src/main/java/pl/coderslab/workshops/taks_manager/Methods.java
@@ -0,0 +1,68 @@
+package pl.coderslab.workshops.taks_manager;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Methods {
+
+    public static void optionsDisplay() {
+        System.out.println(ConsoleColors.BLUE + "Please select an option:");
+
+        String[] opts = {"add", "remove", "list", "exit"};
+        for (int i = 0; i < opts.length; i++) {
+            System.out.println(ConsoleColors.WHITE_BRIGHT + opts[i]);
+        }
+    }
+
+    public static String[][] tasksReader(String fileName) {
+
+        Path path = Paths.get(fileName);
+        if (!Files.exists(path)) {
+            System.out.println("Requested file does not exist!");
+            System.exit(0);
+        }
+
+        String[][] tab = null;
+
+        try {
+            List<String> strings = Files.readAllLines(path);
+            tab = new String[strings.size()][strings.get(0).split(",").length];
+
+            for (int i = 0; i < strings.size(); i++) {
+                String[] split = strings.get(i).split(",");
+                for (int j = 0; j < split.length; j++) {
+                    tab[i][j] = split[j];
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /* Próbowałem to zrobić samemu bez list, ale mi się nie udało:
+        File file = new File(fileName);
+        String[][] tab = null;
+        String str= " ";
+        String[] lines = new String[3];
+
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                for (int i = 0; i < lines.length; i++) {
+                    lines[i] = scanner.nextLine();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Requested file does not exist!");
+        }
+
+        System.out.println(Arrays.toString(lines));
+
+
+         */
+
+        return tab;
+    }
+}
